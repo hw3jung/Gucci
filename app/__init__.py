569b1110a9bee4756ce53361bbaadabd56f2fb47
Olusola Ogunsakin <oaogunsa@uwaterloo.ca>
2013-09-20 12:09:23 -0400

    static files

---
+++ app/__init__.py
@@ -0,0 +1,9 @@
+import os
+from flask import Flask
+from flask import render_template, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def feed(name=None):
+    return render_template('feed.html', name="Sola")
--- app/server.py
+++
@@ -1,9 +0,0 @@
-import os
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def feed():
-    return render_template('feed.html')
-
