2ffb281abb27f071eaa1a14fccd932d68efa9352
Olusola Ogunsakin <oaogunsa@uwaterloo.ca>
2013-09-23 13:06:58 -0400

    io fixes

--- app/static/feed.js
+++ app/static/feed.js
@@ -209,6 +209,7 @@ $(document).ready(function() {
       // feed left view
     },
     kikIt: function() {
+      this.story.kik_count++;
       $.ajax({
         type: 'POST',
         dataType: 'json',
@@ -260,7 +261,6 @@ $(document).ready(function() {
       this.story               = args.story;
       this.slideShowIntervalID = null;
       this.isPrepended         = args.isPrepended;
-      console.log(this.story.kik_count);
     },
     events: {
       'click .image'  : 'goToLink',
@@ -289,12 +289,19 @@ $(document).ready(function() {
       return this;
     },
     kikIt: function() {
+      this.story.kik_count++;
       $.ajax({
         type: 'POST',
         dataType: 'json',
         url: '/api/story/kik',
         data: { 'story_id':  this.story.id }
       });      
+      if(this.story.kik_count == 0) {
+        var html = '<div class="kik-count left"><span>1</span> Kik</div>';
+        this.$('.kik-stats').prepend(html);
+      } else {
+        this.$('.kik-count span').text(this.story.kik_count);
+      }
       if (cards.kik) {
         var link = '';
         if (this.story.link.href) {
